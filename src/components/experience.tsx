936bd2588e79eedd557f737a29fab4adc28d4ade
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-02-22 21:39:06 -0500

    Fix typos and rephrase some sentences

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Experience() {
   return (
     <section className="mt-4">
-      <h2 className="text-2xl font-extrabold border-b-4 mb-2">Experience highlights</h2>
+      <h2 className="text-2xl font-extrabold border-b-4 mb-2">Experience Highlights</h2>
       <ul>
         {cv.experience.map(exp => (
           <li key={exp.duration} className="mb-4">
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -3,7 +3,7 @@ import cv from "@/data/cv.json";
 export default function Skills() {
   return (
     <section className="mt-4">
-      <h2 className="border-b-4 mb-2">Top skills</h2>
+      <h2 className="border-b-4 mb-2">Top Skills</h2>
       <ul className="list-disc pl-8">
         {cv.skills.map(skill => (
           <li key={skill.area}>
